53f34969cf6e869a2af7d28c3d0642b0b4c8194c
Tyler Whitney <tylerjwhitney@gmail.com>
2021-03-24 01:33:06 -0400

    Update setup-web.sh

--- setup-web.sh
+++ setup-web.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-wget 
+wget https://github.com/twhitney11/webd236-replit/raw/main/modules.zip
+unzip modules.zip
+mv modules/* /usr/lib/php/20170718/
+echo "extension=pdo.so" > /etc/php/7.2/cli/conf.d/
 
 php -S 0.0.0.0:8000 router.php
