973ca1f835ff1094140b4bfeab6b0b00e6b3346b
Tyler Whitney <tylerjwhitney@gmail.com>
2021-03-24 01:50:26 -0400

    Update setup-web.sh

--- setup-web.sh
+++ setup-web.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+rm -rf .setup
+mkdir .setup
+cd .setup
 wget https://github.com/twhitney11/webd236-replit/raw/main/modules.zip
 unzip modules.zip
 cd ..
-php -dextension=$PWD/modules/sqlite3.so -dextension=$PWD/modules/pdo_sqlite.so -S 0.0.0.0:8000 router.php
+php -dextension=$PWD/.setup/modules/sqlite3.so -dextension=$PWD/.setup/modules/pdo_sqlite.so -S 0.0.0.0:8000 router.php
