be6e7c4ceafe3bdbaab2e1ae04ed9d8af5a49a01
toddawhittaker <todd@thewhittakers.org>
2021-03-25 14:54:57 -0400

    Clean up database reset code, make quiet

--- setup-web.sh
+++ setup-web.sh
@@ -9,14 +9,19 @@ unzip modules.zip
 mv modules .modules
 rm modules.zip
 
+shopt -s nullglob
+for f in *.sql; do
+	echo "${f%.*}.sql ${%f}"
+done
+
 # Reset the SQLite database
 sqlite3 *.db3 < *.sql
 
 # Cleanup this file when done
-rm -rf setup-web.sh
+# rm -rf setup-web.sh
 
 # Run the local PHP server loading any custom modules and pass everything to router.php
 php -c $PWD/php.ini -dextension=$PWD/.modules/sqlite3.so -dextension=$PWD/.modules/pdo_sqlite.so -S 0.0.0.0:8000 router.php &
 
 # Show logs
-tail -f php_errors.log
+#tail -f php_errors.log
