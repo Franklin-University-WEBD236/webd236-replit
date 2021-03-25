6f3b849d1ccf38e963ec941372831663a638c068
Tyler Whitney <tylerjwhitney@gmail.com>
2021-03-25 00:55:00 -0400

    Update setup-web.sh

--- setup-web.sh
+++ setup-web.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
-rm -rf .setup
-mkdir .setup
-cd .setup
+
+# Remove leftover modules directory
+rm -rf .modules
+
+# Grab any necessary modules from GitHub, unzip them to .modules
 wget https://github.com/twhitney11/webd236-replit/raw/main/modules.zip
 unzip modules.zip
-cd ..
+mv modules .modules
+
+# Reset the SQLite database
+sqlite3 *.db3 < *.sql
+
+# Cleanup this file when done
 rm -rf setup-web.sh
-php -dextension=$PWD/.setup/modules/sqlite3.so -dextension=$PWD/.setup/modules/pdo_sqlite.so -S 0.0.0.0:8000 router.php
+
+# Run the local PHP server loading any custom modules and pass everything to router.php
+php -dextension=$PWD/.modules/sqlite3.so -dextension=$PWD/.modules/pdo_sqlite.so -S 0.0.0.0:8000 router.php
